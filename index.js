b39ae18e415350b670dbce4da3d82d4c57a7afcb
Jonathan MacDuff <jonathan.macduff@outlook.com>
2023-12-11 13:44:42 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random() * 10);
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+let count20 = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+let max = Math.max(count20)
 
 // Write your code here
